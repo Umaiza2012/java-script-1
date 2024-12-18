5c4d220758696899297ed0f38470fc3d0a9269de
Umaiza20112 <hadiawaseem0302@gmail.com>
2024-12-18 19:12:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("r1").innerHTML="Hello, this is my first javascript program"
+//innerHTML is used to update the value
+document.getElementById("r2").innerHTML="I love Coding!"
+
---
+++ main2.js
@@ -0,0 +1,7 @@
+var a=6;
+var b=3;
+var sum=a+b;
+
+document.getElementById("r1").innerHTML=sum;
+
+
